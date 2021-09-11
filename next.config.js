55942e371bac24a980f08fc2e7861eba3a36e366
Matheus Henrique <matheus-alves88@hotmail.com>
2021-09-11 13:29:10 -0300

    Primeiro commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+// next.config.js
+    
 module.exports = {
-  reactStrictMode: true,
-}
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        issuer: {
+          test: /\.(js|ts)x?$/,
+        },
+        
+        use: ['@svgr/webpack'],
+      });
+  
+      return config;
+    },
+  };
---
+++ pages/schedule.js
@@ -0,0 +1,10 @@
+export default function Schedule() {
+    return (
+  
+      <div>
+        Schedule 
+      </div>
+  
+    )
+  }
+  
